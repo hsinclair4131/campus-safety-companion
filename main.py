5ba2a9dbddb4a278194c315231aec29779671d9e
hsinclair4131 <154556451+hsinclair4131@users.noreply.github.com>
2025-11-16 08:59:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+import streamlit as st
+from admin_app.admin_app import admin_dashboard
+from student_app.student_app import student_home
+
+st.set_page_config(page_title="Campus Safety Companion", layout="wide")
+
+st.title("Campus Safety Companion (CSC)")
+
+menu = st.sidebar.selectbox(
+    "Choose App View",
+    ["Student App", "Admin Dashboard"]
+)
+
+if menu == "Student App":
+    student_home()
+
+elif menu == "Admin Dashboard":
+    admin_dashboard()
 
